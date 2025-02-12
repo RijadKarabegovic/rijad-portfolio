f4ed9b3a5767892f219d6dbd1ff5fe332928e937
RijadKarabegovic <rijadkarabegovic091998@hotmail.com>
2025-02-12 18:18:10 +0100

    Test the separate sections in the Home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
   return (
   <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-8xl w-full">
-      <FloatingNav/>
+      {/* <FloatingNav/>
         <Hero />
         <Grid />
         <MyProjects />
         <Work />
-        <Footer />
+        <Footer /> */}
     </div>
   </div>
   );
