4283b5c93a28f4f44e5e9dd30931941b001835a4
RijadKarabegovic <rijadkarabegovic091998@hotmail.com>
2025-02-17 12:50:10 +0100

    ANOTHER fix for the bento grid bug

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -225,11 +225,11 @@ export const BentoGridItem = ({
           {/* add rounded-md h-8 md:h-8, remove rounded-full */}
           {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
           {/* add handleCopy() for the copy the text */}
-          {copied && (
+          {/* {copied && (
             <div className="absolute -bottom-5 right-0">
               <Lottie options={confettiOptions} height={200} width={400} />
             </div>
-          )}
+          )} */}
             <div className="mt-6 lg:mt-10">
             <MagicButton
               title={copied ? "Email is Copied!" : "Copy my email address"}
