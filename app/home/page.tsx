53164a0aa7ea0a58fb6a8f057ccdc55265bd296c
RijadKarabegovic <rijadkarabegovic091998@hotmail.com>
2025-03-09 20:58:39 +0100

    Test for the home page error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,24 @@
+'use client'
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import MyProjects from "@/components/MyProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Work from "@/components/Work";
+
+export default function Home() {
+  return (
+  <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <div className="max-w-8xl w-full">
+      <FloatingNav/>
+        <p>Home</p>
+      <Footer />
+    </div>
+  </div>
+  );
+}
+
+
 // 'use client'
 
 // import { Canvas, useFrame } from '@react-three/fiber'
